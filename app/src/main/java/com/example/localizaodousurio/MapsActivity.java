c6a52c3f36ce9f4af97750e7a624a21b5e3a9ea6
unknown <kleberfort@gmail.com>
2022-11-30 09:56:52 -0300

    Adicionado o código que se encontrava dentro do método de onCreate, códigos como LocationManager que gerencia a localização do usuário, LocationListener que recupera a localização do usário e a CheckSelfPermission para verificar se a permissão foi concedida, eles foram colocados dentro do método onMapReady que executará após o mapa ser carregado.

--- app/src/main/java/com/example/localizaodousurio/MapsActivity.java
+++ app/src/main/java/com/example/localizaodousurio/MapsActivity.java
@@ -45,6 +45,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //Validar permissões
         Permissoes.validarPermissoes(permissoes, this, 1);
 
+
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+
         //Objeto responsável por gerenciar a localização do usuário
         //O método getSystemService recuperamos um serviço do sistema
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
@@ -52,6 +76,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(@NonNull Location location) {
+
+                mMap.clear(); //Limpa o mapa antes de criar os marcadores
+                LatLng meuLocal = new LatLng(location.getLatitude(),location.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(meuLocal).title("Meu local"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(meuLocal, 19));
 
                 Log.d("Localização", "onLocationChanged: " + location.toString());
 
@@ -76,30 +105,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         }
 
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     @Override
